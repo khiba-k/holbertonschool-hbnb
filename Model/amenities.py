7e8dc3854f5f37c7a1c8ff65389b724afa3a5228
khiba-k <khibalpemame@gmail.com>
2024-06-12 16:46:35 +0200

    logic for user class

--- Model/amenities.py
+++ Model/amenities.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 from place import Place
+from user import User
 
 class Amenities(Place):
     def __init__(self, placeName, wifi, pool, extras=None):
-        super().__init__(placeName)
         self.wifi = wifi
         self.pool = pool
         self.extras = [] if extras is None else extras
         
+        # place = Place.taken_places[User.users[placeName[id]]]
+        # place[amenities] = {
+        #     wifi: None,
+        #     pool: True, 
+        #     extras: extras
+        # }
 
     def addExtras(self, *new_extras):
         self.extras.extend(new_extras)
--- Model/user.py
+++ Model/user.py
@@ -1,19 +1,62 @@
 #!/usr/bin/python3
+"""Defines class for User entity"""
 import uuid
+from place import Place
+import DataManager
+
 
 class User:
+    """Handles the users information
+    Attributes:
+        emails []: Has all the existing emails in the system
+        user_places []: Has list of the places the user is hosting
+        user_details {}: Dictionery containing users information 
+    """
+
     emails = []
+    user_places = []
+    user_details = {}
+
     def __init__(self, firstName, lastName, password, email):
+        """Method initializes the User Class instance
+
+        Args:
+            firstName (string): users first name
+            lastName (string): users last name
+            password (string): users password
+            email (string): users email
+        """
+
         self.user_id = uuid.uuid4().hex
         self.firstName =firstName
         self.lastName = lastName
-        self.password = password
+        self.__password = password
         self.email = email
 
+    def add_places(self):
+        """Creates a list of places hosted by the user
+        """
+        if self.user_id == Place.host:
+            User.user_places.append(Place.placeName)
+
+    def add_user(self):
+        """Creates a dictionery of the users information
+        """
+        User.user_details[self.user_id].append(self.firstName)
+        User.user_details[self.user_id].append(self.lastName)
+        User.user_details[self.user_id].append(self.__password)
+        User.user_details[self.user_id].append(self.email)
+        User.user_details[self.user_id].append(User.user_places)
+
+        DataManager.save("user_details")
+
     def add_email(self):
+        """Checks if email entered exists and adds it to list if not true 
+        """
         if self.email not in User.emails:
             User.emails.append(self.email)
         return "Email already exists"
+    
     
 
 
@@ -25,3 +68,6 @@ class User:
             'password': self.password,
             'email': self.email 
         }
+
+
+
