f3b501d94b21400f27c31e19954648b46db1d392
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-06-12 15:49:15 +0200

    add some logic to Place class

--- Model/place.py
+++ Model/place.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-from user import User
+
+"""
+place.py is a module definig the Place class used for adding places
+"""
 from country import Country
 import uuid
 
 class Place():
-    taken_places = []
-    def __init__(self, placeName, description, address, country_name, city_name, lattitude, 
-                 longitude, numberOfRooms, bathrooms, 
-                 pricePerNight, maxGuests):
+    """
+    Defines a place
+
+    Attributes:
+        available_places (object): stores places available for tenants 
+    """
+    available_places = {}
+    def __init__(self, placeName=None, description=None, address=None, country_name=None, city_name=None, lattitude=None, 
+                 longitude=None, numberOfRooms=None, bathrooms=None, 
+                 pricePerNight=None, maxGuests=None):
         self.place_id = uuid.uuid4().hex
         self.placeName = placeName
         self.description = description
@@ -16,39 +25,19 @@ class Place():
         self.city = city_name
         self.lattitude = lattitude
         self.longitude = longitude
-        self.host = User.user_id
         self.numberOfRooms = numberOfRooms
         self.bathrooms = bathrooms
         self.pricePerNight = pricePerNight
         self.maxGuest =maxGuests
         self.amenities = []
 
-    def add_place(self):
-        if self.placeName not in Place.taken_places:
-            Place.taken_places.append(self.placeName)
+    def save_place(self):
+        """
+        add place to data structure
+        """
+        if self.placeName not in Place.available_places:
+            data = dict((self.__dict__))
+            del data["placeName"]
+            Place.available_places[self.placeName] = data
+            return
         return "Name is already taken"
-
-"""
-    @staticmethod
-    def get_available_cities(country_name):
-        country = Country(country_name)
-        return country.get_cities()
-"""
-
-# Example usage:
-if __name__ == "__main__":
-    
-    available_cities = Place.get_available_cities("United States")
-
-    selected_city = available_cities[0]
-
-    place = Place("Luxury Villa", "A beautiful villa with ocean view", 
-                  "123 Ocean Drive", "United States", selected_city,
-                  34.0522, -118.2437, 5, 3, 500, 10)
-
-    print(f"Place: {place.placeName}")
-    print(f"Description: {place.description}")
-    print(f"Address: {place.address}")
-    print(f"Country: {place.country.get_country_name()}")
-    print(f"City: {place.city}")
-    
